062b38ad0a34883ef30a03fbc2884c64077a0224
Jonathan Andrews <ionathas78@hotmail.com>
2020-05-08 12:58:12 -0500

    basic startup logic

---
+++ assets/images/index.js
@@ -0,0 +1,10 @@
+//  exports _cardsList[];
+
+const MAX_CARDS_ITEM = 120;
+const CARD_FILENAME_BASE = ".assets/images/GMA Age of Sail VTT/GMA Age of Sail VTT_Part";
+const CARD_FILENAME_EXTENSION = "jpg"
+const _cardsList = [];
+
+for (var i = 1; i <= MAX_CARDS_ITEM; i++) {
+    cardsList.push(CARD_FILENAME_BASE + i + "." + CARD_FILENAME_EXTENSION);
+}
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,23 @@
-
+const _deckMaster = [];
 
 function getCards() {
-    
+    var groupName = "";
+    //  Get common name between card filenames
+
+    for (var i = 0; i < _cardsList.length; i++) {
+        let cardPath = _cardsList[i];
+        let cardName = cardPath.replace(groupName, "");
+
+        let cardItem = {
+            group: groupName,
+            name: cardName,
+            filePath: cardPath
+        }
+
+        _deckMaster.push(cardItem);
+    }
 }
+
+//      **      Logic
+
+getCards();
